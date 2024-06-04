7cb9146ed875488d604d6ef9da66b538427e4b96
Aaron Carlson <acarlson2@carthage.edu>
2024-06-04 12:50:02 -0500

    assignment complete

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace OperatorExercise
+﻿using Microsoft.VisualBasic;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            Console.WriteLine($"{a}/{b} is {Divide(a,b)} remainder {Modulus(a,b)}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}");
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }
